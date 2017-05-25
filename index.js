073301425f14902e75c08e93fbbb25c35f925897
MichelML <michmoreau.l@gmail.com>
2017-05-25 10:24:58 -0400

    use shelljs

--- index.js
+++ index.js
@@ -1,44 +1,20 @@
 #! /usr/bin/env node
-const spawn = require('child_process').spawn
-const exec = require('child_process').exec
+const logError = (errorMessage) => { console.log(errorMessage); process.exit() }
+const shell = require('shelljs')
+const silent = {silent: true}
+const COMMIT_MESSAGE = process.argv[process.argv.indexOf('-m') + 1]
 
-if (process.argv.length === 2) {
-  console.log('Error: Syntax invalid. Use the following terminal syntax when using hgco:\n\nhgco -m \'your commit message goes here\'\n\n')
-  process.exit()
-}
+if (process.argv.length === 2) logError('Error: Syntax invalid. Use the following terminal syntax when using hgco:\n\nhgco -m \'your commit message goes here\'\n\n')
+if (!COMMIT_MESSAGE) logError('Error: No commit message provided while it is required. Use the following terminal syntax when using hgco:\n\nhgco -m \'your commit message goes here\'\n\n')
 
-exec('hg branch', (error, stdout, stderr) => {
-  if (error) throw error
+shell.exec('hg branch', silent, (code, stdout, stderr) => {
+  if (stderr) logError(stderr)
 
-  const COMMIT_MESSAGE = process.argv[process.argv.indexOf('-m') + 1]
   const JIRA_ISSUE_KEY = stdout.match(/\D{3,4}-\d{1,4}/)
+  if (!JIRA_ISSUE_KEY) logError('Error: No JIRA issue key found in your current branch name. Make sure your branch name contains a JIRA issue key.')
 
-  if (!COMMIT_MESSAGE || !JIRA_ISSUE_KEY) {
-    if (!COMMIT_MESSAGE) {
-      console.log('Error: No commit message provided while it is required. Use the following terminal syntax when using hgco:\n\nhgco -m \'your commit message goes here\'\n\n')
-      process.exit()
-    }
-    if (!JIRA_ISSUE_KEY) {
-      console.log('Error: No JIRA issue key found in your current branch name. Make sure your branch name contains a JIRA issue key.')
-      process.exit()
-    }
-  } else {
-    const hgco = spawn('hg', ['commit', '-m', `${COMMIT_MESSAGE} [${JIRA_ISSUE_KEY}]`], {stdio: 'inherit'})
-
-    process.stdout.on('data', (data) => {
-      console.log(data)
-    })
-
-    process.stderr.on('data', (data) => {
-      console.log(`Error: ${data}`)
-    })
-
-    hgco.on('close', () => {
-      exec('hg log -l 1', (error, stdout, stderr) => {
-        if (error) throw error
-        console.log('\n' + stdout)
-        if (require.main === module) process.exit()
-      })
-    })
-  }
+  const hgco = shell.exec(`hg commit -m '${COMMIT_MESSAGE} [${JIRA_ISSUE_KEY}]'`)
+  console.log('\nLast commit made:\n')
+  const hglog = shell.exec('hg log -l 1')
+  if (require.main === module) process.exit()
 })
